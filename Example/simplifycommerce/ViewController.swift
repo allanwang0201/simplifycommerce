c2f86ab8f99753d6d828671621eed4c7cc513662
Allan Wang <allanwang0201@gmail.com>
2020-04-20 19:18:30 +1000

    Initial commit

---
+++ Example/simplifycommerce/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  simplifycommerce
+//
+//  Created by allanwang0201@gmail.com on 04/20/2020.
+//  Copyright (c) 2020 allanwang0201@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
